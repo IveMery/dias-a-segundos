952f3c030d9341d769b68124a8b89ff04a1a75e0
IvetteMery <ivette.mery@gmail.com>
2020-08-11 17:23:35 -0400

    diasasegundos

---
+++ index.js
@@ -0,0 +1,9 @@
+// dias a segundos
+
+alert ("Debes ingresar  una cantidad de días que quieras y lo pasaremos a segundos")
+let diasSegundos = prompt("Ingresa los minutos que desees convertir")
+
+
+let segundos = (diasSegundos * 86400 )
+
+alert  (`  ${diasSegundos} Días son ${segundos} Segundos `)
